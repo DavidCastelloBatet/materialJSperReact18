fc8926340dadb2a77405300234de81568310058c
David Castello <davidcastellobatetgmail.com>
2022-06-08 21:39:55 +0200

    import/export, find, foreach i filter

--- src/index.js
+++ src/index.js
@@ -1,31 +1,45 @@
-// Desestructuració d'arrays
+// https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/find
+// import per defecte i especific
 
-const panteon = ['Zeus', 'Ares', 'Atenea', 'Afrodita']
-
-const [ , , atenas, ] = panteon;
-console.log(atenas);
-
-
+import heroes, {owners} from "./data/heroes"
+console.log(heroes)
+console.log(owners)
 
 
 
+// busca amb find() el primer que troba
+console.log('************* find *******************')
+const getHeroeById = (id) => heroes.find(hero => hero.id === id)
 
-const retornarArreglo = () => {
-  return ['abc', 123]
+const heroTrobat = getHeroeById(1);
+console.log(heroTrobat)
+
+
+// manera de buscar tots els elements de la condicio amb foreach
+console.log('*********** foreach *********************')
+const heroisPerCompanya = ( companyia ) => {
+  let herois = [];
+
+  heroes.forEach((heroi) => {
+    if(heroi.owner === companyia){
+      herois = [...herois, heroi];
+    }
+  })
+
+  return herois;
+
 }
 
-const [letras, numeros] = retornarArreglo();
-console.log(`Les lletres son ${letras} i els numeros son ${numeros}`);
+console.log(heroisPerCompanya('DC'));
+console.log(heroisPerCompanya('Marvel'));
 
 
 
 
+// manera de buscar tots els elements de la condicio amb filter
+console.log('************* filter *******************')
+const heroisCompanyia  = (companyia) => heroes.filter((heroi) => heroi.owner === companyia)
 
-// simulacio del hook  useState
-const retornarPanteo = ( valor ) => {
-  return [valor, () => { console.log('Hola Panteo') }]
-}
+console.log(heroisCompanyia('DC'));
+console.log(heroisCompanyia('Marvel'));
 
-const [valor, funcio ] = retornarPanteo('Atenea');
-console.log(valor);
-funcio();
